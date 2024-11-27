363370e1952eb695ab190ebae09eb1a4c050a38c
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-11-26 20:51:34 -0500

    finished video 32

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Dinamic url that handles int
+    # url to show All challenges
+    path("", views.index),
+    # Dynamic url that handles int
     path("<int:month>", views.monthly_challenge_by_number),
-    # Dinamic url that handles strings
-    path("<str:month>", views.monthly_challenge)
+    # Dynamic url that handles strings
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "run",
@@ -17,13 +18,32 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
+
+    return HttpResponse(f"<ul>{list_items}</ul>")
+
+
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request,  month):
     try:
         text = monthly_challenges[month]
-        return HttpResponse(text)
+        response_data = f'<h1>{text}</h1>'
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("Not found")
+        return HttpResponseNotFound("<h1>Not found</h1>")
