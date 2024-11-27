f9401b2a1448fae20dde8536cf7dabee8e18ba86
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-11-26 22:05:18 -0500

    finished video 37

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "run",
@@ -43,9 +42,10 @@ def monthly_challenge_by_number(request, month):
 
 def monthly_challenge(request,  month):
     try:
-        text = monthly_challenges[month]
-        # response_data = f'<h1>{text}</h1>'
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        challenge_text = monthly_challenges[month]
+        return render(request, "challenges/challenge.html", {
+            "month": month,
+            "text": challenge_text
+        })
     except:
         return HttpResponseNotFound("<h1>Not found</h1>")
