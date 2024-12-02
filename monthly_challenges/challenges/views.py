ca1e7ff3966494adbf7f37661f46d55c9a1ec21d
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-12-01 21:34:44 -0500

    Finished video 48

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, HttpResponseRedirect, Http404
-from django.template.loader import render_to_string
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 
 monthly_challenges = {
